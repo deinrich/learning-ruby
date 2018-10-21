e07e0876d6028dda7954cf2019542ed90e00f2f0
ga84mus <ga84mus@mytum.de>
2018-10-22 01:07:48 +0300

    ADD Replay feature

--- secret_number.rb
+++ secret_number.rb
@@ -1,20 +1,45 @@
-$secret_number = rand(10)
+require 'English'
+
+$secret_number
 
 $guessed = false
 
-puts "I know a secret number. Try ro guess it!"
+$continue = true
 
 def compare(answer)
-	if $secert_number > answer
-	puts "Secret number is bigger"
-	elsif $secret_number < answer
-	puts "Secret number is smaller"
-	else puts "Secret number is #{$secret_number}. Congrats!"
-	$guessed = true
-	end
+  if $secret_number > answer
+    puts 'Secret number is bigger'
+  elsif $secret_number < answer
+    puts 'Secret number is smaller'
+  else puts "Secret number is #{$secret_number}. Congrats!"
+  $guessed = true
+  end
 end
 
-while $guessed == false
-	gets
-	compare(Integer($_))
+def play
+  $secret_number = rand(10)
+  puts 'I know a secret number. Try ro guess it!'
+  until $guessed
+    gets
+    compare(Integer($LAST_READ_LINE))
+  end
 end
+
+def play_again
+  gets
+  if Integer($LAST_READ_LINE) == 1
+    $guessed = false
+  else $continue = false
+    puts 'It was cool to play with you! Thanks :)'
+  end
+
+end
+
+while $continue
+  play
+  puts 'Do you want to play again?' + "\n" + 'Type 1 for yes and 0 for not'
+  play_again
+end
+
+
+
